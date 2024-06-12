4d14abac7c6cbf201b1edf33e0621b0b70fd442e
arjunmohan500 <arjunmohan500@gmail.com>
2024-06-13 01:01:10 +0530

    created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddBill from './components/AddBill';
 import ViewAll from './components/ViewAll';
+import Search from './components/Search';
 
 function App() {
   return (
     <div>
 <AddBill/>
 <ViewAll/>
+<Search/>
     </div>
     
   );
